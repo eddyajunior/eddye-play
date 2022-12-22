fa01c30e9deb15b8dd6f245c3d56bf1e5ab344ea
Edson Amaral <eddyajunior@gmail.com>
2022-12-22 13:09:01 -0300

    Add Polly test projects

---
+++ src/EddyePlay.Worker.Polly.CircuitBreaker/Models/ResultadoContador.cs
@@ -0,0 +1,8 @@
+namespace EddyePlay.Worker.Polly.CircuitBreaker.Models
+{
+    public class ResultadoContador
+    {
+        public int? ValorAtual { get; set; }
+        public string? Mensagem{ get; set; }
+    }
+}
---
+++ src/EddyePlay.Worker.Polly.CircuitBreaker/Program.cs
@@ -0,0 +1,17 @@
+using EddyePlay.Worker.Polly.CircuitBreaker;
+using EddyePlay.Worker.Polly.CircuitBreaker.Resilience;
+
+// using WorkerConsumoAPIContagem;
+// using WorkerConsumoAPIContagem.Resilience;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddSingleton(CircuitBreakerExtensions.CreatePolicy(
+            Convert.ToInt32(context.Configuration["CircuitBreaker:NumberOfExceptionsBeforeBreaking"]),
+            Convert.ToInt32(context.Configuration["CircuitBreaker:DurationOfBreakInSeconds"])));
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
